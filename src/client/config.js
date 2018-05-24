6c55d653716e9e6ebb4e1f96be4f82f493c3cacc
peterk <tiglek+u16vb@gmail.com>
2018-05-24 21:36:39 +0900

    - fix client config

--- src/client/config.js
+++ src/client/config.js
@@ -1,7 +1,9 @@
+const isDev = process.env.NODE_ENV !== 'production';
+const isBrower = !!process.browser;
 
 export const graphQLPath = '/graphql';
-export const graphiQLPath = '/graphiql';
-export const HOST = 'localhost:3000';
+export const graphiQLPath = isDev ? '/graphiql' : '';
+export const HOST = isBrower ? 'localhost:23000' : 'localhost:3000';
 export const APP_URI = `http://${HOST}`;
 export const GRAPHQL_URL = `${APP_URI}${graphQLPath}`;
 export const GRAPHIQL_URL = `${APP_URI}${graphiQLPath}`;
