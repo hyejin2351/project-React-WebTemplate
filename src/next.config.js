647a276759cc1103201dcd6b9fc9cc6a9e495faa
peterk <tiglek+u16vb@gmail.com>
2018-06-04 20:13:12 +0900

    - fix #12, production build

--- src/next.config.js
+++ src/next.config.js
@@ -1,37 +1,46 @@
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 
-module.exports = {
-  exportPathMap: function exportPathMap() {
-    return {
-      '/': { page: '/' },
-      '/users/profile': { page: '/users/profile' },
-      '/users/create-account': { page: '/users/create-account' },
-      '/users/signin': { page: '/users/signin' },
-    };
-  },
-  serverRuntimeConfig: { // Will only be available on the server side
-    mySecret: 'secret'
-  },
-  publicRuntimeConfig: { // Will be available on both server and client
-    staticFolder: '/static'
-  },
+module.exports = (phase, {defaultConfig}) => {
+  // console.log(phase + '>>>', defaultConfig);
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    defaultConfig.distDir = '../../build/app/client/.next';
+  }
 
-  // webpack: (config, { dev }) => {
-  //   // Perform customizations to webpack config
-  //   if (!dev) {
-  //     config.module.rules.push({
-  //       test: /\.(css|ico|gif)$/,
-  //       use: [
-  //         {
-  //           loader: 'file-loader',
-  //           options: {
-  //             outputPath: 'static/',
-  //           },
-  //         },
-  //       ],
-  //     });
-  //   }
+  return {
+    ...defaultConfig,
+    exportPathMap: function exportPathMap() {
+      return {
+        '/': { page: '/' },
+        '/users/profile': { page: '/users/profile' },
+        '/users/create-account': { page: '/users/create-account' },
+        '/users/signin': { page: '/users/signin' },
+      };
+    },
+    serverRuntimeConfig: { // Will only be available on the server side
+      mySecret: 'secret'
+    },
+    publicRuntimeConfig: { // Will be available on both server and client
+      staticFolder: '/static'
+    },
 
-  //   // Important: return the modified config
-  //   return config;
-  // },
+    // webpack: (config, { dev }) => {
+    //   // Perform customizations to webpack config
+    //   if (!dev) {
+    //     config.module.rules.push({
+    //       test: /\.(css|ico|gif)$/,
+    //       use: [
+    //         {
+    //           loader: 'file-loader',
+    //           options: {
+    //             outputPath: 'static/',
+    //           },
+    //         },
+    //       ],
+    //     });
+    //   }
+
+    //   // Important: return the modified config
+    //   return config;
+    // },
+  }
 };
