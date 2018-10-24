dfb84f4d7487db63e602e7eaea6aaf691b54f910
woon <kjsnewcom@joyfun.kr>
2018-10-24 11:37:17 +0900

    1. admin 비밀번호 변경 적용

--- src/client/pages/admin/passwordChange.js
+++ src/client/pages/admin/passwordChange.js
@@ -6,11 +6,49 @@ import WithAdmin from '../../lib/withAdmin';
 import AdminLayout from '../../layouts/AdminLayout'
 
 import PasswordChangeView from './passwordChange_.jsx';
+
+import redirect from '../../lib/redirect';
+import usersApi from '../../lib/gqlApi/usersApi';
+
 const log = debug('app:passwordChange');
 
 class PasswordChangePage extends React.Component {
-    handleClick(event, client) {
-        log('event: ', event.target.id);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            errors: {},
+            inputData: {
+                curPassword: '',
+                newPassword: '',
+                newConfirmPassword: '',
+            }
+        };
+
+        this.changeData = this.changeData.bind(this);
+        this.changePassword = this.changePassword.bind(this);
+    }
+
+    async changePassword(event, apolloClient) {
+        const { curPassword, newPassword, newConfirmPassword } = this.state.inputData;
+
+        try{
+            const res = await usersApi.changePassword( { apolloClient, curPassword, newPassword} );
+
+            redirect(null, '/admin');
+        } catch (err){
+            log(err);
+        }
+    }
+
+    changeData(event) {
+        const field = event.target.name;
+        const inputData = this.state.inputData;
+        inputData[field] = event.target.value;
+
+        this.setState({
+            inputData
+        });
     }
 
     /**
@@ -22,7 +60,8 @@ class PasswordChangePage extends React.Component {
                 {client => (
                     <AdminLayout apolloClient={client}>
                         <PasswordChangeView
-                            onHandleClick={e=> this.handleClick(e, client)}
+                            onChange={this.changeData}
+                            onSubmit={e => this.changePassword(e, client)}
                         />
                     </AdminLayout>
                 )}
